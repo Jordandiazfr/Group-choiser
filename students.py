f972ba6996a00219016093ebde89bf75cdd7a220
Jordandiazfr <jordandiazfr@gmail.com>
2021-01-16 19:37:01 +0100

    BUG FIXED:  # if there's only one left, there is a valueError since simple needs two or three.

--- students.py
+++ students.py
@@ -28,7 +28,9 @@ class Students:
         self.names = self.members
         self.len = len(names_list)
         self.max_marked_members = 1
+        self.sub_group = []
         self.group = {}
+
 
     def is_marked(self, member) -> bool:
         """Determine if a student has the + mark so it cannot be paired with others with the same mark"""
@@ -51,22 +53,25 @@ class Students:
     # TO FIXXXXX START #
 
     def make_sub_group_marked(self, num_of_members) -> list:
+# Input make a group of 2 persons
         """Makes a little group with conforming the num_of_members input and taking in consideration the + mark"""
-        marked_sub_group = []
         marked = 0
-        while len(marked_sub_group) < num_of_members:
-            # Select a random name
+        marked_sub_group = []
+        # Get the total number of marked people:
+        total_marked = self.num_of_marked()  #1
+        for i in range(20):
+            # Until we dont have two members
             selection = random.choice(self.names)
-            # Determine if the student is marked and there is not marked students in the group
-            if self.is_marked(selection) and marked < self.max_marked_members:
+            print(selection)
+            if self.is_marked(selection) and marked < 1:
                 marked_sub_group.append(selection)
                 self.delete(selection)
                 marked += 1
-            # Determine if the student is NOT marked and there is at least one marked students in the group
-            elif self.is_marked(selection) is False and marked > 0:
+            if marked > 0:
                 marked_sub_group.append(selection)
-                self.delete(selection)
-                #  *************  BUG : NEEDS AT LEAST ONE MARKED STUDENT PER GROUP  , FOR  4 GROUPS, 4 MARKED STUDENTS #
+            if len(marked_sub_group) == num_of_members:
+                break
+# Expected  output, marked person with one unmarked
         return marked_sub_group
 
     # TO FIXXXXX  END #
@@ -74,24 +79,23 @@ class Students:
     def make_group_marked(self, number_of_members):
         """ Make the final group of students supposing there is at least one marked """
         number_of_groups = self.len // number_of_members
-        for i in range(1,number_of_groups):
+        for i in range(1, number_of_groups):
             self.group['Group #'+str(i)] = self.make_sub_group_marked(number_of_members)
         return self.group
 
     def make_group_not_marked(self, number_of_members):
         """ Make the group of students when there is not marked students """
-        sub_group = []
         n_of_group = 1
-        while len(self.names) > 0:
-            print(number_of_members)
-            print(self.names)
-            # *************  BUG ... if there's only one left, there is a valueError since simple needs two or three.
-            picked = random.sample(self.names, k=number_of_members)
-            sub_group.append(picked)
-            self.group['Group #' + str(n_of_group)] = sub_group[n_of_group - 1]
+        while self.names:
+            for _ in range(number_of_members):
+                if not self.names:
+                    break
+                picked = random.choice(self.names)
+                self.sub_group.append(picked)
+                self.delete(picked)
+            self.group['Group #' + str(n_of_group)] = self.sub_group
+            self.sub_group = []
             n_of_group += 1
-            for pick in picked:
-                self.delete(pick)
         return self.group
 
 
@@ -99,7 +103,6 @@ class Students:
         """ This is the final group creator, it will detect if the group has a marked member or not and call
         the right method """
         marked = self.num_of_marked()
-        print(marked)
         if marked == 0:
             total = self.make_group_not_marked(number_of_members)
         if marked > 0:
